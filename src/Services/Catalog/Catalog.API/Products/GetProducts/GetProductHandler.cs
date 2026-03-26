186b0cefb0b68720f57ff3fdbf1879982380897c
Abhishek-111 <abhishekbmwcbz@gmail.com>
2026-03-26 10:51:16 +0530

    Implemented GetProducts

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductHandler.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery(): IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+    public class GetProductQueryHandler(
+        IDocumentSession session,
+        ILogger<GetProductQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle is called with {@Query}");
+            
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            return new GetProductsResult(products);
+        }
+    }
+}
+ 
